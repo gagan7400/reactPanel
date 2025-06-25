5b52a8739d296a6fe866b6abbaa70f2a3876c28d
gagan7400 <102296159+gagan7400@users.noreply.github.com>
2025-06-25 13:33:20 +0530

    updatechild

--- src/context/Child.jsx
+++ src/context/Child.jsx
@@ -6,9 +6,9 @@ export default function Child() {
     let data = useContext(usersData);
     let midddata = useContext(middcontext);
     return (
-        
+
         <div className='bg-info p-5 m-auto w-75 border'>
-            <h4> Child Comp</h4>
+            <h4> Child Comp hello new data</h4>
             <h5>midd context : {midddata}</h5>
             {data.users[0].name}
             <button onClick={() => { data.setUsers([{ name: "rajesh" }]) }}>update</button>
